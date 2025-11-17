a453e679eb578430c36c6934df5e31c44579b188
elidistro7 <elidistro@gmail.com>
2025-11-17 11:32:30 +0300

    new design

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -47,12 +47,7 @@ export default function PrivacyPolicy() {
         </div>
       </main>
 
-      {/* Footer */}
-      <footer className="border-t border-brand-border bg-white mt-12">
-        <div className="container mx-auto px-4 py-6 text-center text-sm text-gray-600 font-poppins">
-          © {new Date().getFullYear()} SasaNDIO Sasa. All rights reserved.
-        </div>
-      </footer>
+  
     </div>
   );
 }
